c1598c0a9e8b53e28c227e8a2a3fee8a8d433bc2
M.Hafizh Trinelzy <92729726+21voidx@users.noreply.github.com>
2026-08-09 00:34:36 +0700

    Delete transform.py

--- transform.py
+++
@@ -1,7 +0,0 @@
-def calculate_net_revenue(gross_revenue, discount):
-    return gross_revenue - discount
-
-
-if __name__ == "__main__":
-    result = calculate_net_revenue(100_000, 20_000)
-    print(f"Net revenue: {result}")
